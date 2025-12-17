284a651bda1022287354fcd8006c0a8ce629d8e1
c8geek <72575716+415geek@users.noreply.github.com>
2025-12-17 15:47:44 -0800

    Create aurainsight_typography.py

---
+++ aurainsight_typography.py
@@ -0,0 +1,11 @@
+# aurainsight_typography.py
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+def register_aurainsight_fonts():
+    pdfmetrics.registerFont(TTFont("Noto", "assets/fonts/NotoSansSC-Regular.ttf"))
+    pdfmetrics.registerFont(TTFont("Noto-Bold", "assets/fonts/NotoSansSC-Bold.ttf"))
+    pdfmetrics.registerFont(TTFont("Roboto", "assets/fonts/Roboto-Regular.ttf"))
+    pdfmetrics.registerFont(TTFont("Roboto-Bold", "assets/fonts/Roboto-Bold.ttf"))
+    pdfmetrics.registerFont(TTFont("Roboto-Italic", "assets/fonts/Roboto-Italic.ttf"))
