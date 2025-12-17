c5f4f283188a4ef7960053f635e2dcf3b0742d95
c8geek <72575716+415geek@users.noreply.github.com>
2025-12-17 15:49:43 -0800

    Refactor font registration for readability

--- aurainsight_typography.py
+++ aurainsight_typography.py
@@ -4,8 +4,18 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
 def register_aurainsight_fonts():
-    pdfmetrics.registerFont(TTFont("Noto", "assets/fonts/NotoSansSC-Regular.ttf"))
-    pdfmetrics.registerFont(TTFont("Noto-Bold", "assets/fonts/NotoSansSC-Bold.ttf"))
-    pdfmetrics.registerFont(TTFont("Roboto", "assets/fonts/Roboto-Regular.ttf"))
-    pdfmetrics.registerFont(TTFont("Roboto-Bold", "assets/fonts/Roboto-Bold.ttf"))
-    pdfmetrics.registerFont(TTFont("Roboto-Italic", "assets/fonts/Roboto-Italic.ttf"))
+    pdfmetrics.registerFont(
+        TTFont("Noto", "assets/fonts/NotoSansSC-Regular.ttf")
+    )
+    pdfmetrics.registerFont(
+        TTFont("Noto-Bold", "assets/fonts/NotoSansSC-Bold.ttf")
+    )
+    pdfmetrics.registerFont(
+        TTFont("Roboto", "assets/fonts/Roboto-Regular.ttf")
+    )
+    pdfmetrics.registerFont(
+        TTFont("Roboto-Bold", "assets/fonts/Roboto-Bold.ttf")
+    )
+    pdfmetrics.registerFont(
+        TTFont("Roboto-Italic", "assets/fonts/Roboto-Italic.ttf")
+    )
